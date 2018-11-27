dc8388e39d810a7960af2021ed1cedcf1065d279
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-26 23:28:35 -0500

    Update waitForButtonPress.sh

--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -7,33 +7,33 @@
 #(pressed). The goal here is that this shell should wait until the button is
 #pressed and then released
 
-pinVal=$( gpio read 5 ); #here  we read the button and save the value
+pinValue=$( gpio read 5 ); #here  we read the button and save the value
 pressed=1; #not pressed by default
 while true
 do
 		
-	if [ $pinVal -eq 1 ] ; #if pin is not pressed, wait until it is pressed,
+	if [ $pinValue -eq 1 ] ; #if pin is not pressed, wait until it is pressed,
 	then
-		until [ $pinVal -eq 0 ] #until the pin value changes, keep looping
+		until [ $pinValue -eq 0 ] #until the pin value changes, keep looping
 		do
-			echo $pinVal;
+			echo $pinValue;
 			pressed=0
 			export pressed;
-			pinVal=$( gpio read 5 )
+			pinValue=$( gpio read 5 )
 		done
 		
 	fi
 	
-	pinVal=$( gpio read 5 )
+	pinValue=$( gpio read 5 )
 	
-	if [ $pinVal -eq 0 ] ; #if pin is  pressed, wait until it is not pressed,
+	if [ $pinValue -eq 0 ] ; #if pin is  pressed, wait until it is not pressed,
 	then
-		until [ $pinVal -eq 1 ] #until the pin value changes, keep looping
+		until [ $pinValue -eq 1 ] #until the pin value changes, keep looping
 		do
-			echo $pinVal;
+			echo $pinValue;
 			pressed=1
 			export pressed;			
-			pinVal=$( gpio read 5 )
+			pinValue=$( gpio read 5 )
 		done
 		
 	fi

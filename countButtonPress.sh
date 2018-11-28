c7d2b75580afab9f94764e5368b373787b8630a6
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-28 09:20:24 -0500

    Update countButtonPress.sh

--- countButtonPress.sh
+++ countButtonPress.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-COUNTER=0; #counter increments every time button is pressed
+COUNTER=0; 
 
-pinValue=$( gpio read 5 ); #here  we read the button and save the value
+pinValue=$( gpio read 5 ); 
 
 	until [ $COUNTER -eq 16 ]
 	do
-		#sleep .5s;
-		pinValue=$( gpio read 5 ); #here  we read the button and save the value
+		pinValue=$( gpio read 5 ); 
 		sleep .1s;
 
 		if [ $pinValue -eq 0 ]
@@ -24,8 +23,8 @@ pinValue=$( gpio read 5 ); #here  we read the button and save the value
 		if [ $COUNTER -eq 16 ]
 		then	
 			echo BUZZER
-			gpio write 4 1; #here  buzzer goes ON
+			gpio write 4 1;
 			sleep 1s;
-			gpio write 4 0; #here  buzzer goes off
+			gpio write 4 0; 
 
 		fi		

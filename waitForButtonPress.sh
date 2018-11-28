810deb7f1bf0e7f363bd6b9dd4f753546c127bab
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-28 09:18:57 -0500

    Update waitForButtonPress.sh

--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 
-
-#Write a shell script that monitors the value of gpio
-#pin 5. It should busy wait (continually check and loop) as long as the pin
-#is 1 (not pressed). It should then busy wait as long as the pin is 0
-#(pressed). The goal here is that this shell should wait until the button is
-#pressed and then released
-
-pinValue=$( gpio read 5 ); #here  we read the button and save the value
-pressed=1; #not pressed by default
-while true
+pin = ( gpio read 5 ); 
+press = 1;
+while 1
 do
 		
-	if [ $pinValue -eq 1 ] ; #if pin is not pressed, wait until it is pressed,
+	if [ $pin -eq 1 ] ; 
 	then
-		until [ $pinValue -eq 0 ] #until the pin value changes, keep looping
+		until [ $pin -eq 0 ]
 		do
-			echo $pinValue;
-			pressed=0
-			export pressed;
-			pinValue=$( gpio read 5 )
+			echo $pin;
+			press=0
+			export press;
+			pin=$( gpio read 5 )
 		done
 		
 	fi
 	
-	pinValue=$( gpio read 5 )
+	pin=$( gpio read 5 )
 	
-	if [ $pinValue -eq 0 ] ; #if pin is  pressed, wait until it is not pressed,
+	if [ $pin -eq 0 ] ; 
 	then
-		until [ $pinValue -eq 1 ] #until the pin value changes, keep looping
+
+		until [ $pin -eq 1 ] 
 		do
-			echo $pinValue;
-			pressed=1
-			export pressed;			
-			pinValue=$( gpio read 5 )
+			echo $pin;
+			press=1
+			export press;			
+			pin=$( gpio read 5 )
 		done
 		
 	fi

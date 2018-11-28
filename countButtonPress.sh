6aec0835b5873ae2a9a372ee1db906b4646a1d3c
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-28 09:49:00 -0500

    Update countButtonPress.sh

--- countButtonPress.sh
+++ countButtonPress.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
-
 count=0
-last=1
-
-while((1))
+pin=$(gpio read 5)
+if [ "$count" -eq 0 ]
+then ./initMode.sh
+fi
+while true
 do
-	let "now=$(gpio read 5)"
-	if [ $now == $last ]; then
-		sleep 0.01s
-	else
-		if [ $now == 1 ]; then
-			let "last=$now"
-		else
-			((count++))
-			./setbits.sh $count
-			echo "button pressed $count times"
-			let "last=$now"
-		fi
-	fi
-	
-	if [ $count == 16 ]; then
-		gpio write 4 1
-		echo "number too big"
-		sleep 1s
-		gpio write 4 0
-		count=0
-		echo "reset number to 0"
-	fi
-done
+    pin=$(gpio read 5)
+    if [ "$pin" -eq 0 ]
+    then
+        
+        ((count++))
+	./waitForButtonPress.sh
+	echo "$count"
+        ./setbits.sh "$count"
 
+        if [ "$count" -eq 16 ]
+        then
+            a=count
+        fi
+
+        if [ "$count" -eq 0 ]
+        then
+            gpio write 4 1 
+            sleep 1
+            gpio write 4 0 
+	    echo "Stop"
+	    break
+        fi
+    fi
+done

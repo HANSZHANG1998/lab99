7c460b6052ae63ac74291c8154683825d2a58050
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-28 09:50:01 -0500

    Update waitForButtonPress.sh

--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -1,35 +1,11 @@
-#!/bin/bash
-
-pin = ( gpio read 5 ); 
-press = 1;
-while 1
+until [ $(gpio read 5) -eq 0 ]
 do
-		
-	if [ $pin -eq 1 ] ; 
-	then
-		until [ $pin -eq 0 ]
-		do
-			echo $pin;
-			press=0
-			export press;
-			pin=$( gpio read 5 )
-		done
-		
-	fi
-	
-	pin=$( gpio read 5 )
-	
-	if [ $pin -eq 0 ] ; 
-	then
-
-		until [ $pin -eq 1 ] 
-		do
-			echo $pin;
-			press=1
-			export press;			
-			pin=$( gpio read 5 )
-		done
-		
-	fi
-	
+	echo "button not pressed"
+	sleep 0.25
 done
+
+ until [ $(gpio read 5) -eq 1 ]
+  do
+        echo "button pressed"
+	sleep 0.25 		
+ done

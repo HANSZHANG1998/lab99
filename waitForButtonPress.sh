c89330711402b5ca0c6c4e5f84faed2bdccd8a43
HANSZHANG1998 <43784847+HANSZHANG1998@users.noreply.github.com>
2018-11-28 09:57:19 -0500

    Update waitForButtonPress.sh

--- waitForButtonPress.sh
+++ waitForButtonPress.sh
@@ -1,11 +1,11 @@
 until [ $(gpio read 5) -eq 0 ]
 do
 	echo "button not pressed"
-	sleep 0.25
+	sleep 2
 done
 
  until [ $(gpio read 5) -eq 1 ]
   do
         echo "button pressed"
-	sleep 0.25 		
+	sleep 2		
  done
